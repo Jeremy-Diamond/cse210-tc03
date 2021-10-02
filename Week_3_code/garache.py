e3a889b8d50ca0e5735425f62ed1f4dab122d83e
rgarache <82833214+rgarache@users.noreply.github.com>
2021-10-01 22:00:11 -0600

    Update garache.py
    Adding a function

--- Week_3_code/garache.py
+++ Week_3_code/garache.py
@@ -1,3 +1,11 @@
 
 
 print("Hello World")
+
+def multiply_by_5(number):
+    print(f'{number} * 5 = {number * 5}')
+print('Start the program')    
+multiply_by_5(7)
+print('Next')
+multiply_by_5(113)
+print('Fin')
